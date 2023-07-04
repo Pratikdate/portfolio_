01adc0d2cd8ea38a246d92235e4cc5fe35ba64ec
Pratik date <pratikdate100@gmail.com>
2023-07-04 16:22:19 +0530

    Delete my_profile_ directory

--- my_profile_/my-app/2022-css-only-typewriter-text-animation/css-only-typewriter-text-animation/README.md
+++
@@ -1,4 +0,0 @@
-# CSS-only typewriter text animation
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/alvaromontoro/pen/rNwVpdd](https://codepen.io/alvaromontoro/pen/rNwVpdd).
-
--- my_profile_/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["C:/Django/my_profile/my-app/public/{index.html,app.js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
